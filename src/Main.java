bd7ba495e5864d2aef8ca4c34ae19b181d7ce6df
kalinychev <alex.kalinychev@yandex.ru>
2022-11-05 11:19:23 +0300

    commit 14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, прекрасный world");
+        System.out.println("Привет, мир. Как я рад что получилось сохранить изменения");
     }
 }
